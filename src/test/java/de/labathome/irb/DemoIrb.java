5856d1ec03be806d0d6ad4fb5efbb586a86b8ec0
Jonathan Schilling <jonathan.schilling@mail.de>
2024-07-07 13:32:17 +0200

    read all test file - no compressed ones among them!

--- src/test/java/de/labathome/irb/DemoIrb.java
+++ src/test/java/de/labathome/irb/DemoIrb.java
@@ -9,7 +9,27 @@ public class DemoIrb {
 	public static void main(String[] args) throws IOException {
 
 		final String folder = new File(DemoIrb.class.getClassLoader().getResource("de/labathome/irb").getFile()).getAbsolutePath();
-		final String filename = folder + "/140203AB/AB020300.irb";
+
+		// uncompressed
+//		final String filename = folder + "/140114AA/AA011400.irb";
+//		final String filename = folder + "/140114AA/AA011401.irb";
+
+//		final String filename = folder + "/140115AA/AA011500.irb";
+//		final String filename = folder + "/140115AA/AA011501.irb";
+//		final String filename = folder + "/140115AA/AA011502.irb";
+//		final String filename = folder + "/140115AA/AA011503.irb";
+
+//		final String filename = folder + "/140202AA/AA020200.irb";
+
+//		final String filename = folder + "/140203AA/AA020300.irb";
+//		final String filename = folder + "/140203AA/AA020301.irb";
+//		final String filename = folder + "/140203AA/AA020302.irb";
+//		final String filename = folder + "/140203AA/AA020303.irb";
+//		final String filename = folder + "/140203AA/AA020304.irb";
+
+//		final String filename = folder + "/140203AB/AB020300.irb";
+//		final String filename = folder + "/140203AB/AB020301.irb";
+		final String filename = folder + "/140203AB/AB020302.irb";
 
 		IrbFile irbFile = IrbFile.fromFile(filename);
 
